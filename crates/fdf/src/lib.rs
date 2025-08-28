0041cd892456bf6ace1f1b775e504d95405b026b
Aarni Koskela <akx@iki.fi>
2025-08-28 20:15:53 +0300

    Decouple progress

--- crates/fdf/src/lib.rs
+++ crates/fdf/src/lib.rs
@@ -4,7 +4,9 @@ pub mod hashwork;
 pub mod interrupt;
 pub mod options;
 pub mod output;
+pub mod progress;
 
 pub use find::{AugDirEntry, GroupKey, KeyToDentsMap, KeyToStringToDentMap};
 pub use options::{HashAlgorithm, NameGroupingOption, Options};
 pub use output::{FindStats, GrandResult, HashGroupResult, HashStats, KeyGroupResult};
+pub use progress::{ProgressCallback, ProgressEvent};
